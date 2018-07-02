386dee4470ad3bd590573b2849304a1dfce9e9e3
jacklife <40485020+jacklife@users.noreply.github.com>
2018-07-02 18:48:19 +0800

    Create ESConst.java

---
+++ elasticsearch/src/main/java/com/es/domain/ESConst.java
@@ -0,0 +1,19 @@
+package com.es.domain;
+
+public class ESConst {
+    public static final String CLUSTER_NAME = "cluster.name";
+
+    public static final String SPAN_INDEX = "span*";
+    public static final String SPAN_TYPE = "span";
+
+    public static final String TRANSACTIONID = "transactionId";
+    public static final String APP_NAME = "appName";
+    public static final String SEQUENCE = "traceTransactionSequence";
+    public static final String PARENT_ID = "parentSpanId";
+    public static final String START_TIME = "startTime";
+    public static final String URL = "rpc";
+    public static final String TIER = "traceAgentId";
+    public static final String ERRORS = "errCode";
+    public static final String MAX_ELAPSED = "elapsed";
+    public static final String AVG_ELAPSED = "elapsed";
+}
